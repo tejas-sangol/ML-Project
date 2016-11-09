f17ccb9572d7e2b8e648185c7c8cb81ac3914a04
tejas <f2014774@goa.bits-pilani.ac.in>
2016-11-09 23:46:35 +0530

    new_set check_sliding_window

--- slide_window2.py
+++ slide_window2.py
@@ -1,29 +1,31 @@
 from skimage import io,transform,feature,color,data
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
+import pickle,os;
 from multiprocessing import Pool
 from skimage.transform import pyramid_gaussian,pyramid_expand
+import sys;
 def crop_image(arr,start_row,end_row,start_coloumn,end_coloumn):
 	return arr[start_coloumn:end_coloumn,start_row:end_row];
 # string.decode('utf-8')
-loaded_model = pickle.load(open('./dumps/mlp','rb'))
-pred = [0,0,0,0]
-max_image = None;
+loaded_model = pickle.load(open('./dumps/svm','rb'));
+# with open('./multiclass_mlp','rb') as f:
+# 	import pickle;
+# 	m_svm = pickle.load(f);
+
 # iterates over first 10 images
-def run(i):
-	j_value =i;
-	entry = open('./training_data/bounding_boxes.csv').read().split('\n')[i].split(',')
-	name = entry[0];
-	image = io.imread('./training_data/raw/3/'+name)
-	pred = [0,0,0,0]
+def run(arg):
+	pred=[0,0,0,0];
+	symbol,dir,entry = arg;
+	image = io.imread('./dataset/'+ entry.split(',')[0]);
+
 	#downscaled images
 	downscale_pyramid = pyramid_gaussian(image,downscale = 1.1) # generator
 
 	#upscaled images
 	upscale_pyramid = []
 	im = image
-	for j in range(0,10):
+	for j in range(0,5):
 		im = pyramid_expand(im,upscale = 1.1)
 		upscale_pyramid.append(im)
 
@@ -56,28 +58,43 @@ def run(i):
 					cropped_image=crop_image(image,i,i+128,j,j+128);
 		a = a/1.1
 
-	with open('./multiclass_mlp','rb') as f:
-		import pickle;
-		m_svm = pickle.load(f);
+	io.imsave('./test/'+str(dir)+'/'+symbol+'.jpg',cropped_image);
 
-	print m_svm.predict([max_image]);
-	io.imsave('check'+str(j_value)+'.jpg',cropped_image);
-	entry[1:] = map(int,entry[1:]);
+	entry = map(int,entry.split(',')[1:]);
+	#
+	# #Intersection over Union error calculation
+	actual = [entry[0],entry[1],entry[2]-entry[0]] # [x,y,size]
+	# # pred = pred[:3]
+	#
+	# cord1 = [max(actual[0],pred[0]),max(actual[1],pred[1])]
+	# cord2 = [min(actual[0]+actual[2],pred[0]+pred[2]),min(actual[1]+actual[2],pred[1]+pred[2])]
+	#
+	# intersectionArea = (cord2[0]-cord1[0])*(cord2[1]-cord1[1])
+	# unionArea = actual[2]*actual[2] + pred[2]*pred[2] - intersectionArea
+	#
+	# iou  = float(intersectionArea)/unionArea;
+	#
 
-	#Intersection over Union error calculation
-	actual = [entry[1],entry[2],entry[3]-entry[1]] # [x,y,size]
-	# pred = pred[:3]
+	# prediction = m_svm.predict_proba([max_image])[0];
+	# pred_max = max(prediction);
+	# pred_index = [i for i in range(len(prediction)) if prediction[i]==pred_max ][0] + 1;
 
-	cord1 = [max(actual[0],pred[0]),max(actual[1],pred[1])]
-	cord2 = [min(actual[0]+actual[2],pred[0]+pred[2]),min(actual[1]+actual[2],pred[1]+pred[2])]
-
-	intersectionArea = (cord2[0]-cord1[0])*(cord2[1]-cord1[1])
-	unionArea = actual[2]*actual[2] + pred[2]*pred[2] - intersectionArea
-
-	error = float(intersectionArea)/unionArea
-
-	print (pred,actual,error)
+	print dir+'/'+symbol,pred,actual,symbol[0]
+	sys.stdout.flush();
 
 p=Pool(4);
-p.map(run,[i for i in range(401,411)]);
-# run(611);
+
+args=[];
+for root,directories,_ in os.walk('dataset'):
+	for dir in directories:
+		if dir[0]=='.' : continue;
+		with open('./dataset/'+dir+'/'+dir+'_loc.csv') as file_list:
+			file_list = file_list.read().split('\n')[1:];
+
+			for entry in file_list:
+				if len(entry)==0:continue;
+				symbol = entry[entry.find('/')+1:entry.find('/')+3];
+				if int(symbol[1])<8: continue;
+				args.append((symbol,dir,entry));
+p.map(run,args);
+# run(('A8', 'user_10', 'user_10/A8.jpg,195,61,305,171'));
